59b0506d68902108970c616afa26181cc5fe98db
sanuv9683 <sanuv9683@gmail.com>
2025-05-14 21:27:30 +0100

    login is finalized

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent {
   username = '';
   password = '';
   error = '';
+  focused = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -18,5 +19,12 @@ export class LoginComponent {
     this.authService.login(this.username, this.password)
       .then(() => this.router.navigate(['/dashboard/manager']))
       .catch(err => this.error = 'Invalid credentials.');
+  }
+
+
+
+  onBlur(event: FocusEvent) {
+    const input = event.target as HTMLInputElement;
+    this.focused = !!input.value;
   }
 }
